b301ad8d71b17d3c0a184ba971f0259d9377454f
Thomas <tcowie1@uncc.edu>
2021-11-30 17:14:34 -0500

    removing data_stats

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 import streamlit as st
 from multiapp import MultiApp
-from apps import home, data_stats, loans, salaries # import your app modules here
+from apps import home, loans, salaries # import your app modules here
 
 app = MultiApp()
 
 # Add all your application here
 app.add_app("Home", home.app)
-app.add_app("Data Stats", data_stats.app)
 app.add_app("Loans", loans.app)
 app.add_app("Salaries", salaries.app)
 
