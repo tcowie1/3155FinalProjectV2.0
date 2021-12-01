55c9a4cf7cb556ba0b6f44fa292de4b87ccd32ac
Thomas <tcowie1@uncc.edu>
2021-11-30 19:07:52 -0500

    messages

--- apps/loans.py
+++ apps/loans.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+import math
+import altair as alt
+from collections import namedtuple
 from data.create_data import create_table
 
 #We gotta make sure to put the data in the data folder and then import it correctly.
@@ -8,10 +11,8 @@ from data.create_data import create_table
 def app():
     st.title('Loans')
 
-    st.write("This is a sample data stats in the mutliapp.")
-    st.write("See `apps/data_stats.py` to know how to use it.")
+    st.write("Loans are something most students will have to face when going to college, however, many students especially at the highschool age aren't aware of the true cost of a loan they take out for college.")
+    st.write("This simple calculator will show you what a loan can cost you by the time you pay it off.")
 
     st.markdown("### Plot Data")
-    df = create_table()
-
-    st.line_chart(df)
+    
