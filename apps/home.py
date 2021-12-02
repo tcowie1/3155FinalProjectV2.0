a1f3cb5b350731f521fdbeea5bc74c5ca87e401a
Thomas <tcowie1@uncc.edu>
2021-12-02 09:21:18 -0500

    update

--- apps/home.py
+++ apps/home.py
@@ -6,11 +6,11 @@ import altair as alt
 from collections import namedtuple
 from data.create_data import create_table
 
-def app():
-    #Page Config
+ #Page Config
     #st.set_page_config(initial_sidebar_state="collapsed")
     #st.set_page_config(page_title='UniSight', page_icon=":goat:")  #These dont work with multipage. Need to investigate.
 
+def app():
     #Title and Intro
     st.title('UniSight')
     st.write("At Unisight, we want to provide accessable, easy-to-read data and visuals to help potential and current students make informed decisions on college degrees and the careers they lead to. Guiding individuals through major college and career decisions, with up-to-date information, is our objective.")
