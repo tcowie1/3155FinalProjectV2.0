ccc191076209a9c6d0635bcf61b6391803b18f21
Thomas <tcowie1@uncc.edu>
2021-12-02 09:18:48 -0500

    pagetitle + goat

--- apps/home.py
+++ apps/home.py
@@ -8,6 +8,7 @@ from data.create_data import create_table
 
 def app():
     st.set_page_config(initial_sidebar_state="collapsed")
+    st.set_page_config(page_title='UniSight', page_icon=":goat:")
     st.title('UniSight')
     st.write("At Unisight, we want to provide accessable, easy-to-read data and visuals to help potential and current students make informed decisions on college degrees and the careers they lead to. Guiding individuals through major college and career decisions, with up-to-date information, is our objective.")
 
