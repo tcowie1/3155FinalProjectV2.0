8ea990ba095a06561bbee4e09a0f0078bf693c71
Thomas <tcowie1@uncc.edu>
2021-12-02 09:35:13 -0500

    fix

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ from multiapp import MultiApp
 from apps import home, loans, salaries # import your app modules here
 
 #Page Config
-    #st.set_page_config(initial_sidebar_state="collapsed")
-    #st.set_page_config(page_title='UniSight', page_icon=":goat:")  #These dont work with multipage. Need to investigate.
-    
+st.set_page_config(initial_sidebar_state="collapsed")
+st.set_page_config(page_title='UniSight', page_icon=":goat:")  #These dont work with multipage. Need to investigate.
+
 app = MultiApp()
 
 # Add all your application here
