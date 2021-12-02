c655b60d20ec7cb7307ae5e31bd59fad067816ae
Thomas <tcowie1@uncc.edu>
2021-12-02 17:45:27 -0500

    adding pages

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 st.set_page_config(initial_sidebar_state="collapsed")
 #st.set_page_config(page_title='UniSight', page_icon=":goat:")
 from multiapp import MultiApp
-from apps import home, loans, salaries # import your app modules here
+from apps import home, loans, salaries, about, data_stats # import your app modules here
 
 app = MultiApp()
 
@@ -10,6 +10,8 @@ app = MultiApp()
 app.add_app("Home", home.app)
 app.add_app("Loans", loans.app)
 app.add_app("Salaries", salaries.app)
+app.add_app("Data Stats, data_stats.app")
+app.add_app("About", about.app)
 
 # The main app
 app.run()
