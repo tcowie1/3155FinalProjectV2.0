8f3864bb0fa1a74a2abfade10e3e47b50feb9185
Thomas <tcowie1@uncc.edu>
2021-12-02 09:17:06 -0500

    sidebar collapse

--- apps/home.py
+++ apps/home.py
@@ -7,8 +7,9 @@ from collections import namedtuple
 from data.create_data import create_table
 
 def app():
+    st.set_page_config(initial_sidebar_state="collapsed")
     st.title('UniSight')
     st.write("At Unisight, we want to provide accessable, easy-to-read data and visuals to help potential and current students make informed decisions on college degrees and the careers they lead to. Guiding individuals through major college and career decisions, with up-to-date information, is our objective.")
 
-    
+
 
