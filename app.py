5eadda9552265f7cbe60a0f8045df12c62b84ad0
Thomas <tcowie1@uncc.edu>
2021-12-02 18:40:47 -0500

    revert

--- app.py
+++ app.py
@@ -1,17 +1,13 @@
 import streamlit as st
+#Page Config
 st.set_page_config(initial_sidebar_state="collapsed")
 #st.set_page_config(page_title='UniSight', page_icon=":goat:")
 from multiapp import MultiApp
-from apps import home, loans, salaries, about, data_stats # import your app modules here
-
+from apps import home, loans, salaries # import your app modules here
 app = MultiApp()
-
 # Add all your application here
 app.add_app("Home", home.app)
 app.add_app("Loans", loans.app)
 app.add_app("Salaries", salaries.app)
-app.add_app("Data Stats", data_stats.app)
-app.add_app("About", about.app)
-
 # The main app
 app.run()
