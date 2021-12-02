d7d61d1728e59e22844f54b91085bac2841a9bc6
Thomas <tcowie1@uncc.edu>
2021-12-02 09:35:52 -0500

    fix

--- app.py
+++ app.py
@@ -1,11 +1,10 @@
 import streamlit as st
-
+#Page Config
+st.set_page_config(initial_sidebar_state="collapsed")
 from multiapp import MultiApp
 from apps import home, loans, salaries # import your app modules here
 
-#Page Config
-st.set_page_config(initial_sidebar_state="collapsed")
-st.set_page_config(page_title='UniSight', page_icon=":goat:")  #These dont work with multipage. Need to investigate.
+#st.set_page_config(page_title='UniSight', page_icon=":goat:")  #These dont work with multipage. Need to investigate.
 
 app = MultiApp()
 
